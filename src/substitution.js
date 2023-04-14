c5ab455a019770a12aa83cbb801ae6480bcaa7b9
vallif247 <123528849+vallif247@users.noreply.github.com>
2023-04-14 17:23:04 -0400

    Removed comments

--- src/substitution.js
+++ src/substitution.js
@@ -1,8 +1,3 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const substitutionModule = (function () {
   
 const orderAlph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
