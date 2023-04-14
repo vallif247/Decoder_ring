e611192bc93adbf8c8fdc8485a8aa8899a925463
vallif247 <123528849+vallif247@users.noreply.github.com>
2023-04-14 17:22:02 -0400

    Removed unnecessary comments

--- src/caesar.js
+++ src/caesar.js
@@ -1,11 +1,4 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-  /*const letters = {a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8, i:9, j:10, k:11, l:12, m:13, n:14, o:15, p:16, q:17, r:18, s:19, t:20, u:21, v:22, w:23, x:24, y:25, z:26}*/
   const letters = {1:"a", 2:"b", 3:"c", 4:"d", 5:"e", 6:"f", 7:"g", 8:"h", 9:"i", 10:"j", 11:"k", 12:"l", 13:"m", 14:"n", 15:"o", 16:"p", 17:"q", 18:"r", 19:"s", 20:"t", 21:"u", 22:"v", 23:"w", 24:"x", 25:"y", 26:"z"}
  function caesar(input, shift, encode = true) {
     // your solution code here
@@ -15,14 +8,11 @@ const caesarModule = (function () {
     //turn input into numbers
     let charNumber = []
     for (let i = 0; i < input.length; i++) {
-      //if character is a letter      console.log(input[i])
-      //console.log(input[i].toUpperCase().charCodeAt(0))
       if (input[i].toUpperCase().charCodeAt(0) > 64 && input[i].toUpperCase().charCodeAt(0) < 91) {
       charNumber.push(input[i].toUpperCase().charCodeAt(0))
       } else charNumber.push(input[i])
-      //if it's not a letter push it as is
     }
-    //turn the large charCode number into a number in letters object with shift
+   
     const arrayNumber = []
     if (encode == true) {
     charNumber.forEach((number) => {
@@ -56,9 +46,7 @@ const caesarModule = (function () {
    } else acc += letters[letter]
    return acc
  }, [])
-
     return arrayLetter
-
   } 
 } 
 
